483314367ce956d6e0358f899881ef95cd9cd3ad
ItaloNCcosta <ItaloNCcosta@gmail.com>
2024-10-03 13:48:14 -0300

    feat(docker): adding docker composer and dependencies

---
+++ app/Livewire/Calendar/IndexCalendar.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Calendar;
+
+use Livewire\Component;
+
+class IndexCalendar extends Component
+{
+    public function render()
+    {
+        return view('livewire.calendar.index-calendar');
+    }
+}
